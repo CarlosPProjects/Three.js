f9a777d201e0396b20f80bc72735b2217b06c1b9
cg9822@gmail.com <cg9822@gmail.com>
2022-05-29 22:44:17 +0200

    4__OrbitControls

---
+++ 5__Resize/js/app.js
@@ -0,0 +1,57 @@
+import * as THREE from "./three.module.js";
+
+// --------------- CREATING SCENE --------------
+var scene = new THREE.Scene();
+scene.background = new THREE.Color(0x2a3b4c);
+
+// ------------------- ADD CAMERA -------------
+var camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight
+);
+
+// ---------------- RENDERER ----------------
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// ----------------- ADD GEOMETRY ----------------
+var geometry = new THREE.BoxGeometry();
+var material = new THREE.MeshBasicMaterial({
+  color: 0x00ff00,
+  wireframe: true,
+});
+var cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+camera.position.z = 5;
+
+// ------------- ADDING LISTENERS -------------
+window.addEventListener("resize", redirect, false);
+
+function redirect() {
+  // Camera frustum aspect ratio
+  camera.aspect = window.innerWidth / window.innerHeight;
+
+  // Every time we change the camera settings, we need to update the projection matrix
+  camera.updateProjectionMatrix();
+
+  // Let's set the new size
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // At this point we should render again the whole new settings.
+  renderer.render(scene, camera);
+}
+
+// ---------------- ANIMATION ----------------
+var animate = function () {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+};
+
+animate();
